adb18bf0c49eab8821a7c899b0c85feb16cc9063
Stainley <szwtron@gmail.com>
2021-08-27 10:24:26 +0700

    Pengumpulan Week 2

---
+++ Week2/app/src/main/java/id/ac/umn/week02_00000036396/FirstActivity.java
@@ -0,0 +1,19 @@
+package id.ac.umn.week02_00000036396;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first);
+
+    }
+}
