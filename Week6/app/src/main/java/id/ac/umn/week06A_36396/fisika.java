375889e35e8bd017b35c0371ba645dcb42411e30
Stainley <szwtron@gmail.com>
2021-09-21 11:57:21 +0700

    Pengumpulan Week 6

---
+++ Week6/app/src/main/java/id/ac/umn/week06A_36396/fisika.java
@@ -0,0 +1,14 @@
+package id.ac.umn.week06A_36396;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class fisika extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fisika);
+    }
+}
